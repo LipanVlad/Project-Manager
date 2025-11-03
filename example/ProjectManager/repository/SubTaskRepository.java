bb024f4d8147763ed17daae2e26d25cc7b5395fc
LipanVlad <lipanvlad1405@gmail.com>
2025-11-03 15:53:23 +0200

    Delete example/ProjectManager directory

--- example/ProjectManager/repository/SubTaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ProjectManager.repository;
-
-import com.example.ProjectManager.entity.SubTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubTaskRepository extends JpaRepository<SubTask, Long> {
-
-}
--- example/ProjectManager/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ProjectManager.repository;
-
-import com.example.ProjectManager.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
